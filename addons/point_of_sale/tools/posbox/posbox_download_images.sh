3b7b758da073540e328e1d91532da78a926d703a
Damien <ddaly@isi.nc>
2017-10-03 08:10:20 +1100

    [FIX] Posbox image build with latest Raspbian Jessie

--- addons/point_of_sale/tools/posbox/posbox_download_images.sh
+++ addons/point_of_sale/tools/posbox/posbox_download_images.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-wget 'https://downloads.raspberrypi.org/raspbian_lite_latest' -O raspbian.img.zip
+#latest jessie
+IMAGEURL=https://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2017-07-05/2017-07-05-raspbian-jessie-lite.zip
+#stretch
+#IMAGEURL=https://downloads.raspberrypi.org/raspbian_lite_latest
+
+wget "${IMAGEURL}" -O raspbian.img.zip
 unzip raspbian.img.zip
 wget 'https://github.com/dhruvvyas90/qemu-rpi-kernel/raw/master/kernel-qemu-4.4.13-jessie' -O kernel-qemu
